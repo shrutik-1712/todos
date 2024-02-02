ac5c4be2b7a961366b66a91b1c4daad92365396a
Shrutik Dawane <53071103+shrutik-1712@users.noreply.github.com>
2024-02-02 16:04:34 +0530

    Add files via upload

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,49 +1,10 @@
-import LoginPage from "../pages/LoginPage"
-import TodoPage from "../pages/TodoPage"
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
-import RequiedAuth from "./RequiedAuth"
-import SignupPage from "../pages/SignupPage"
-import Logout from "../pages/Logout"
-import AuthStore from "../../stores/authStore"
+
+import NavBar from "./NavBar"
 function App() {
- const store=AuthStore();
+ 
   return (
     <>
-    <BrowserRouter>
-    <nav className="bg-gray-800">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex h-16 items-center justify-between">
-    
-        <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-        <div className="flex flex-shrink-0 items-center">
-          <div className="hidden sm:ml-6 sm:block">
-            <div className="flex space-x-4">
-        <Link to="/"><span className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium">home</span></Link>
-      
-      
-        <Link to="/LoginPage"><span className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium">login</span></Link>
-      
-        <Link to="/signup"><span className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium">Signup</span></Link>
-     
-        <Link to="/Logout"><span className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium">logout</span></Link>
-        </div>
-        </div>
-        </div>
-        </div>
-        </div>
-    </div>
-    </nav> 
-      
-    <Routes>
-      <Route index element={<RequiedAuth>
-        <TodoPage/>
-      </RequiedAuth>}/>
-      <Route path="/LoginPage" element={<LoginPage/>}/>
-      <Route path="/signup" element={<SignupPage/>}/>
-      <Route path="/Logout" element={<Logout/>}/>
-    </Routes>
-    </BrowserRouter>
-
+    <NavBar/>
     </>
   )
 }
